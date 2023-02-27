2869480b48a801269ce6b8e9707cffb7f5eeb98e
Clarance Liberiste Ntwari <liberintwari@gmail.com>
2023-02-27 08:34:36 -0800

    chore: intial setup

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+    app: {
+        head: {
+          charset: 'utf-16',
+          viewport: 'width=500, initial-scale=1',
+          title: 'StunPark',
+          meta: [
+            // <meta name="description" content="My amazing site">
+            // TODO: seo improvement
+            { name: 'description', content: '' }
+          ],
+        }
+      },
+    modules: [
+        '@nuxtjs/tailwindcss',
+        ["@storyblok/nuxt", { accessToken: "<your-access-token>" }]
+      ]
 
 })
