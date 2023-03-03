689bdad44bc91b14181f0ac85e1df70d44e7b02a
Clarance Liberiste Ntwari <liberintwari@gmail.com>
2023-03-03 06:23:26 -0800

    feat: button

--- components/atoms/button/SButton.stories.ts
+++ components/atoms/button/SButton.stories.ts
@@ -66,3 +66,33 @@ export const Secondary = Template.bind({})
 Secondary.args = {
     appearance: "secondary",
 }
+
+const ExampleTemplate: StoryFn = (args) => ({
+    components: { Button },
+    setup() {
+        return { args }
+    },
+    template: `
+        <div style="background-color:#f7f7f7;padding:1rem 2rem">
+            <div style="margin:1rem 0.5rem"> <Button width="full" > button </Button>  </div>
+            <div style="margin:1rem 0.5rem"> <Button width="full" appearance="secondary" > button </Button>  </div>
+            <div style="margin:1rem 0.5rem;">
+                <Button style="margin:1rem 0.5rem;" appearance="primary" > button </Button>
+                <Button style="margin:1rem 0.5rem;" appearance="secondary" > button </Button>
+                <Button style="margin:1rem 0.5rem;" appearance="text" > button </Button>
+            </div>
+            <div >
+                <Button style="margin:1rem 0.5rem;" appearance="primary" rounded="md" > button </Button>
+                <Button style="margin:1rem 0.5rem;" appearance="secondary" rounded="md"> button </Button>
+                <Button style="margin:1rem 0.5rem;" appearance="text" rounded="md"> button </Button>
+            </div>
+            <div style="margin:1rem 0.5rem;">
+                <Button style="margin:1rem 0.5rem;" appearance="primary" rounded="none" > button </Button>
+                <Button style="margin:1rem 0.5rem;" appearance="secondary" rounded="none"> button </Button>
+                <Button style="margin:1rem 0.5rem;" appearance="text" rounded="none"> button </Button>
+            </div>
+        </div>
+    `,
+})
+
+export const Example = ExampleTemplate.bind({})
