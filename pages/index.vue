3cc99697aa224b829dc3dd81245672f06f10c3b1
Clarance Liberiste Ntwari <60586899+claranceliberi@users.noreply.github.com>
2023-06-29 16:45:07 +0200

    component

--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,25 @@
 <script lang="ts" setup>
-const colorMode = useColorMode()
-
-const isDark = computed({
-    get() {
-        return colorMode.value === "dark"
-    },
-    set() {
-        colorMode.preference = colorMode.value === "dark" ? "light" : "dark"
-    },
-})
+const page = ref(1)
+const items = ref(Array(55))
 </script>
 
 <template>
     <div>
         <h1>home page</h1>
-        <ClientOnly>
-            <UButton :icon="isDark ? 'i-heroicons-moon-20-solid' : 'i-heroicons-sun-20-solid'" color="gray" variant="ghost" aria-label="Theme" @click="isDark = !isDark" />
 
-            <template #fallback>
-                <div class="w-8 h-8" />
-            </template>
-        </ClientOnly>
+        <UPagination
+            v-model="page"
+            :total="items.length"
+            :ui="{
+                wrapper: 'flex items-center gap-1',
+                rounded: 'rounded-full min-w-[32px] justify-center',
+            }"
+            :prev-button="null"
+            :next-button="{
+                icon: 'i-heroicons-arrow-small-right-20-solid',
+                color: 'primary',
+                variant: 'outline',
+            }"
+        />
     </div>
 </template>
