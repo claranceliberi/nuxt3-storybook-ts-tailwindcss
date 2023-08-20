28af2b07400374c9adc1422bf53e6d7a9aa809c4
Clarance Liberiste Ntwari <60586899+claranceliberi@users.noreply.github.com>
2023-08-20 23:23:52 +0200

    chore: add pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ["vite-plugin-vue-type-imports/nuxt", "@nuxthq/ui"],
-
+    modules: ["vite-plugin-vue-type-imports/nuxt", "@nuxthq/ui",'@pinia/nuxt'],
     app: {
         head: {
             charset: "utf-16",
             viewport: "width=500, initial-scale=1",
             title: "Nuxt App",
             meta: [
-                // <meta name="description" content="My amazing site">
-                // TODO: seo improvement
-                { name: "description", content: "" },
+                { name: "description", content: "My amazing site" },
             ],
         },
     },
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,18 @@
 <script lang="ts" setup>
+import { useCounterStore } from '../store/index';
+
 const page = ref(1)
 const items = ref(Array(55))
+
+const store = useCounterStore()
+
 </script>
 
 <template>
     <div>
         <h1>home page</h1>
+
+        <p>{{ store.name }}</p>
 
         <UPagination
             v-model="page"
