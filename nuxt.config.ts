99e541c91f836d893c0a421be39267a3b835516c
Clarance Liberiste Ntwari <60586899+claranceliberi@users.noreply.github.com>
2023-08-20 22:59:26 +0200

    feat: dev tools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     modules: ["vite-plugin-vue-type-imports/nuxt", "@nuxthq/ui"],
+
     app: {
         head: {
             charset: "utf-16",
             viewport: "width=500, initial-scale=1",
-            title: "StunPark",
+            title: "Nuxt App",
             meta: [
                 // <meta name="description" content="My amazing site">
                 // TODO: seo improvement
@@ -13,17 +14,34 @@ export default defineNuxtConfig({
             ],
         },
     },
+
     typescript: {
         strict: true,
     },
+
     components: [
         {
             path: "~~/components",
             pathPrefix: false,
         },
     ],
+
     ui: {
         global: true,
         icons: ["heroicons", "simple-icons"],
+    },
+
+    devtools: {
+      enabled: true,
+
+      timeline: {
+        enabled: true,
+      },
+
+      vscode: {
+        // startOnBoot: true,
+        // enabled: true,
+        mode: "tunnel",
+    },
     },
 })
